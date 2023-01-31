0e874f9307ddccf03b0f4efcdff152f484e88d73
Kostya Sterkhov <sterxovk@mail.ru>
2023-01-31 20:26:52 +0400

    create slider control

--- app.js
+++ app.js
@@ -6,19 +6,44 @@ let hero_slide_index = 0;
 
 let hero_slide_play = true;
 
+let hero_slide_control_items = hero_slide.querySelectorAll('.slide-control-item')
+
+let slide_next = hero_slide.querySelector('.slide-next')
+
+let slide_prev = hero_slide.querySelector('.slide-prev')
+
 showSlide = (index) => {
   hero_slide.querySelector('.slide.active').classList.remove('active');
+  hero_slide.querySelector('.slide-control-item.active').classList.remove('active');
+  hero_slide_control_items[index].classList.add('active')
   hero_slide_items[index].classList.add('active');
 };
 
 nextSlide = () => {
-  hero_slide_index =
-    hero_slide_index + 1 === hero_slide_items.length ? 0 : hero_slide_index + 1;
+  console.log('erw')
+  hero_slide_index = hero_slide_index + 1 === hero_slide_items.length ? 0 : hero_slide_index + 1;
   showSlide(hero_slide_index);
 };
 
-//pause slide when mouse come in slider
-hero_slide.addEventListener('mouseover', () )
+prevSlide = () => {
+  hero_slide_index = hero_slide_index - 1 < 0 ? hero_slide_items.length - 1 : hero_slide_index - 1;
+  showSlide(hero_slide_index);
+};
+
+slide_next.addEventListener('click', () => nextSlide())
+
+slide_prev.addEventListener('click', () => prevSlide())
+
+//add event to slide select
+hero_slide_control_items.forEach((item, index) => {
+    item.addEventListener('click', () => showSlide(index))
+})
+
+// pause slide when mouse come in slider
+hero_slide.addEventListener('mouseover', () => hero_slide_play = false)
+
+// resume slide when mouse leave out slide
+hero_slide.addEventListener('mouseleave', () => hero_slide_play = true)
 
 setTimeout(() => {
   hero_slide_items[0].classList.add('active');
@@ -27,7 +52,7 @@ setTimeout(() => {
 
 // auto slide
 
-setInterval(() => {
-    if (!hero_slide_play) return
-    nextSlide()
-}, 5000)
+// setInterval(() => {
+//     if (!hero_slide_play) return
+//     nextSlide()
+// }, 5000)
